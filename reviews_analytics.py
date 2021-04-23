5e48f7561369783a80b4b368f3f9431fd6cb7cb1
Fish <fishhuang068@gmail.com>
2021-04-24 01:14:57 +0800

    add filter good

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -19,10 +19,17 @@ for d in data:
     #print(sum_len)
 print('留言的平均長度為', sum_len / len(data))
 
+#---------------------
 new = []
 for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 
-
+#---------------------
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
